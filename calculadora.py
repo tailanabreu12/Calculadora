6802f76e1548f8fab78fc9d49ed69da57a29b992
Tailan <tailanabreuads@gmail.com>
2022-08-10 00:34:35 -0500

    #4 O usuário deverá realizar novo cálculo sem executar a aplicação novamente. E mudança no formato de saída de todas as operações.

--- calculadora.py
+++ calculadora.py
@@ -1,31 +1,44 @@
-operation = input('''
-Digite a operação matemática que deseja:
-+ Adicao
-- Subtracao
-* Multiplicacao
-/ Divisao
-''')
+def calculate():
+    operation = input('''
+    ================ WEB ACADEMY ==================
+    == Digite a operação matemática que deseja:  ==
+    == + Adicao                                  == 
+    == - Subtracao                               ==
+    == * Multiplicacao                           ==   
+    == / Divisao                                 ==
+    : ''')
 
-number_1 = int(input('Entre com o primeiro numero: '))
-number_2 = int(input('Entre com o segundo numero: '))
+    number_1 = int(input('Entre com o primeiro numero: '))
+    number_2 = int(input('Entre com o segundo numero: '))
+    #adicao
+    if operation == '+':
+        print(f'O resultado de {number_1} + {number_2} é ', number_1 + number_2)
 
-if operation == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
+    #subtração
+    elif operation == '-':
+        print(f'O resultado de {number_1} - {number_2} é ', number_1 - number_2)
 
-#subtração
-elif operation == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
+    #multiplicação
+    elif operation == '*':
+        print(f'O resultado de {number_1} * {number_2} é ', number_1 * number_2)
 
-#multiplicação
-elif operation == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+    #divisão
+    elif operation == '/':
+        print(f'O resultado de {number_1} / {number_2} é ', number_1 / number_2)
+    else:
+        print('Voce não escolheu uma operação válida, tente novamente.')
+    again()
 
-#divisão
-elif operation == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
-else:
-    print('Voce não escolheu uma operação válida, tente novamente.')
+def again():
+    calc_again = input('''
+ Deseja calcular novamente?
+    
+ Selecione S para SIM ou N para NAO: ''')
+    if calc_again.upper() == 'S':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('Até mais!! :-)')
+    else:
+        again()
+
+calculate()
