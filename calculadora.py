1a0106d68f87dcfcfdc9fe465f92cca8f1193249
Tailan <tailanabreuads@gmail.com>
2022-08-10 00:00:25 -0500

    #3 - Permitir que o usuário selecione a operação que deseja

--- calculadora.py
+++ calculadora.py
@@ -1,17 +1,31 @@
+operation = input('''
+Digite a operação matemática que deseja:
++ Adicao
+- Subtracao
+* Multiplicacao
+/ Divisao
+''')
+
 number_1 = int(input('Entre com o primeiro numero: '))
 number_2 = int(input('Entre com o segundo numero: '))
 
-print('{} + {} = '.format(number_1, number_2))
-print(number_1 + number_2)
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
 
 #subtração
-print('{} - {} = '.format(number_1, number_2))
-print(number_1 - number_2)
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
 
 #multiplicação
-print('{} * {} = '.format(number_1, number_2))
-print(number_1 * number_2)
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
 
 #divisão
-print('{} / {} = '.format(number_1, number_2))
-print(number_1 / number_2)
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+else:
+    print('Voce não escolheu uma operação válida, tente novamente.')
